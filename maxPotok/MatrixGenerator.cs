9e8cfce4438ea957947e15cc36312df3e1cfd8bb
LocalUser <LocalUser@K3-504-09>
2018-11-22 09:31:14 +0500

    Добавьте файлы проекта.

---
+++ maxPotok/MatrixGenerator.cs
@@ -0,0 +1,139 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maxPotok
+{
+    // Класс для генерации матриц
+    public static class MatrixGenerator
+    {
+        static Random rnd = new Random();
+
+        // Метод для случайной генерации матрицы расстояний
+        public static int[,] GenerateRandom(int N)
+        {
+            // Матрица для заполнения
+            int[,] Matrix = new int[N, N];
+            // середина матрицы
+            int mid = N / 2;
+            // величина максимального потока
+            int s = 0;
+
+            // заполнение второй части матрицы
+            for (int i = 0; i < mid; i++)
+                for (int j = mid; j < N; j++)
+                {
+                    Matrix[i, j] = rnd.Next(1, 4);
+                    s += Matrix[i, j];
+                }
+
+            // заполнение первой части матрицы
+            for (int i = 0; i < mid; i++)
+                for (int j = i + 1; j < mid; j++)
+                    Matrix[i, j] = rnd.Next(s, s + 10);
+
+            // заполнение третьей части матрицы
+            for (int i = mid; i < N; i++)
+                for (int j = i + 1; j < N; j++)
+                    Matrix[i, j] = rnd.Next(s, s + 10);
+
+            // Перемешивание строк и столбцов матрицы
+            for (int k = 0; k < N / 3; k++)
+            {
+                // Выбор вершин, которые будут переставлены
+                int first = rnd.Next(1, N - 1), second = rnd.Next(1, N - 1);
+
+                int temp;
+                // Перестановка столбцов
+                for (int i = 0; i < N; i++)
+                {
+                    temp = Matrix[i, first];
+                    Matrix[i, first] = Matrix[i, second];
+                    Matrix[i, second] = temp;
+                }
+                // Перестановка строк
+                for (int j = 0; j < N; j++)
+                {
+                    temp = Matrix[first, j];
+                    Matrix[first, j] = Matrix[second, j];
+                    Matrix[second, j] = temp;
+                }
+            }
+
+            // вывод созданной матрицы
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write("{0, -4}", Matrix[i, j]);
+                }
+            }
+
+            Console.WriteLine("\n" + "Величина максимального потока (генератор): " + s);
+            return Matrix;
+        }
+
+        // Метод для ручной генерации матрицы расстояний
+        public static int[,] GenerateInput(int N)
+        {
+            // Матрица для заполнения
+            int[,] Matrix = new int[N, N];
+            // середина матрицы
+            int mid = N / 2;
+            // величина максимального потока
+            int s = 0;
+            string[] str;
+
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine("Введите {0} строку матрицы: ", i + 1);
+                str = Console.ReadLine().Split(' ');
+                for (int j = 0; j < str.Length; j++)
+                {
+                    Matrix[i, j] = Int32.Parse(str[j]);
+                    //if (i < mid && j >= mid)
+                    //    s += Int32.Parse(str[j]);
+                }
+            }
+
+            //// Перемешивание строк и столбцов матрицы
+            //for (int k = 0; k < N / 3; k++)
+            //{
+            //    // Выбор вершин, которые будут переставлены
+            //    int first = rnd.Next(1, N - 1), second = rnd.Next(1, N - 1);
+
+            //    int temp;
+            //    // Перестановка столбцов
+            //    for (int i = 0; i < N; i++)
+            //    {
+            //        temp = Matrix[i, first];
+            //        Matrix[i, first] = Matrix[i, second];
+            //        Matrix[i, second] = temp;
+            //    }
+            //    // Перестановка строк
+            //    for (int j = 0; j < N; j++)
+            //    {
+            //        temp = Matrix[first, j];
+            //        Matrix[first, j] = Matrix[second, j];
+            //        Matrix[second, j] = temp;
+            //    }
+            //}
+
+            // вывод созданной матрицы
+            for (int i = 0; i < N; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < N; j++)
+                {
+                    Console.Write("{0, -4}", Matrix[i, j]);
+                }
+            }
+
+            //Console.WriteLine("\n" + "Величина максимального потока (генератор): " + s);
+            return Matrix;
+        }
+    }
+}
---
+++ maxPotok/Program.cs
@@ -0,0 +1,162 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace maxPotok
+{
+    class Program
+    {
+        // посещенные вершины
+        static List<int> visited = new List<int>();
+        // путь от s до t
+        static Stack<int> path = new Stack<int>();
+        // матрица
+        public static int[,] matrix;
+        // кол-во вершин в графе
+        static int n;
+        // минимальное значение в пути
+        static int min = int.MaxValue;
+        // начальная, конечная вершины
+        public static int s = 0, t;
+
+        // поиск пути от s до t
+        public static bool DFS(int node)
+        {
+            // вершина посещена
+            visited.Add(node);
+            // для каждой смежной вершины к заданной
+            for (int nextNode = 0; nextNode < n; nextNode++)
+            {
+                if (node == nextNode)
+                    continue;
+                // если следующая вершина не посещена и не является концевой
+                if (!visited.Contains(nextNode) && matrix[node, nextNode] != 0)
+                {
+                    // если следующая вершина является стоком
+                    if (nextNode == t || DFS(nextNode))
+                    {
+                        // добавляем вершину в путь
+                        path.Push(nextNode);
+                        return true;
+                    }
+                }
+            }
+            return false;
+        }
+
+        // поиск минимального значения в пути
+        static void Min()
+        {
+            // начало ребра
+            int nodeStart = 0;
+            // конец ребра
+            int nodeEnd;
+            // кол-во вершин в пути
+            int c = path.Count();
+
+            for (int i = 0; i < c; i++)
+            {
+                nodeEnd = path.ElementAt(i);
+                    min = Math.Min(Math.Abs(min), Math.Abs(matrix[nodeStart, nodeEnd]));
+                nodeStart = nodeEnd;
+            }
+        }
+
+        // поиск матрицы пропускных способностей
+        static void MaxFlow()
+        {
+            if (!DFS(0))
+                return;
+            int nodeStart = 0;
+            int nodeEnd;
+            int c = path.Count();
+            Min();
+
+            for (int i = 0; i < c; i++)
+            {
+                nodeEnd = path.Pop();
+                //Console.Write(nodeEnd + " ");
+                if (matrix[nodeStart, nodeEnd] > 0)
+                {
+                    matrix[nodeStart, nodeEnd] -= min;
+                    matrix[nodeEnd, nodeStart] -= min;
+                }
+                if (matrix[nodeStart, nodeEnd] < 0)
+                {
+                    matrix[nodeStart, nodeEnd] += min;
+                    matrix[nodeEnd, nodeStart] += min;
+                }
+                nodeStart = nodeEnd;
+            }
+            //Console.WriteLine();
+            visited.Clear();
+            min = int.MaxValue;
+            MaxFlow();
+        }
+
+        // величина максимального потока
+        static int ValueMaxFlow()
+        {
+            int m = 0;
+            for (int i = 0; i < n; i++)
+                m += matrix[n - 1, i];
+            return Math.Abs(m);
+        }
+
+        // вывод матрицы максимального потока
+        static void PrintMatrMaxFlow()
+        {
+            Console.WriteLine();
+            Console.WriteLine("Максимальный поток");
+            for (int i = 0; i < n; i++)
+            {
+                Console.WriteLine();
+                for (int j = 0; j < n; j++)
+                {
+                    if (matrix[j, i] > 0)
+                        Console.Write("0   ");
+                    if (matrix[j, i] <= 0)
+                        Console.Write("{0, -4}", Math.Abs(matrix[j, i]).ToString());
+                }
+            }
+            Console.WriteLine("\n" + "Величина максимального потока: " + ValueMaxFlow());
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Введите n");
+            n = Convert.ToInt32(Console.ReadLine());
+            t = n - 1;
+            matrix = new int[n, n];
+            bool ok = false;
+            do
+            {
+                Console.WriteLine("1 - генерация случайной матрицы");
+                Console.WriteLine("2 - ручной ввод матрицы");
+                var Result = Console.ReadKey();
+                switch (Result.Key)
+                {
+                    case ConsoleKey.D1:
+                    case ConsoleKey.NumPad1:
+                        matrix = MatrixGenerator.GenerateRandom(n);
+                        ok = true;
+                        break;
+                    case ConsoleKey.D2:
+                    case ConsoleKey.NumPad2:
+                        matrix = MatrixGenerator.GenerateInput(n);
+                        ok = true;
+                        break;
+                    default:
+                        ok = false;
+                        break;
+                }
+            }while (!ok);
+            MaxFlow();
+            PrintMatrMaxFlow();
+
+            Console.ReadLine();
+        }
+    }
+}
